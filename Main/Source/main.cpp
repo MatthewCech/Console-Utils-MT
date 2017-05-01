c92b7b295d83d1b84e95ce67e88ff1ebc21d7f9c
Johannes P <joshadow@gmail.com>
2017-05-01 00:50:21 -0700

    removed cursor

--- Main/Source/main.cpp
+++ Main/Source/main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char** argv)
   UNUSED(argc);
   UNUSED(argv);
 
+  printf("\033[2J");
+  printf("\033[?25l");
   DoSinglethreadTest();
-  
+  printf("\033[?25h");
+
   return 0;
 }
