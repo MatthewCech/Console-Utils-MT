0967bd77683ca7350f91ac83927b3ead62dd169c
FOGBOUND-ALPHA\reverie <contact@rw0.pw>
2017-04-30 18:59:47 -0700

    = Converted frame to use two buffers.

--- Test_08_MTWindowing/Frame.cpp
+++ Test_08_MTWindowing/Frame.cpp
@@ -7,24 +7,96 @@
 // Non-default Ctor, takes an offset and a size + unique ID.
 // ID can NOT be changed post-construction.
 Frame::Frame(int id, int x, int y, int width, int height ) 
- : PosX(x)
- , PosY(y)
- , FrameWidth(width)
- , FrameHeight(height)
+ : _posX(x)
+ , _posY(y)
+ , _width(width)
+ , _height(height)
  , _id(id)
- , _bufferSize(width * height * (StepSize))
- , _buffer(new char[_bufferSize])
+ , _bufferSize(width * height)
+ , _bufferChar(new char[_bufferSize])
+ , _bufferAttributes(new Attribute[_bufferSize])
+ {  }
+
+
+Frame::~Frame()
 {
-  for(int i = 0; i < _bufferSize; i += StepSize)
-    memcpy(_buffer + i, "\033[22;32m!", StepSize);
+  delete[] _bufferChar;
+  delete[] _bufferAttributes;
 }
 
-// Writes a 
-void Frame::Write(unsigned char c, int x, int y, ColorRGB)
+// Writes char <c> to position (<x>, <y>) and gives the
+// character color <fg>, and the background the color <bg>.
+void Frame::Write(char c, int x, int y, ColorRGB fg, ColorRGB bg)
 {
-  char buff[StepSize];
-  sprintf(buff, "\033[22;31H%c", c); // Insert color specific drawing here.
-  memcpy(_buffer + (FrameWidth* x + y), buff, StepSize);
+  Write(&c, 1, x, y, fg, bg);
+}
+
+// Writes a buffer of <size> starting at position (<x>, <y>) 
+// and gives the character color <fg>, and the background the color <bg>.
+// User must guarentee <buf> is at least <size> large. Undefined
+// behavior may result otherwise.
+void Frame::Write(char *buf, int size, int x, int y, ColorRGB fg, ColorRGB bg)
+{
+  // No drawing outside the frame.
+  if(!inRange(x, 0, _width - 1))
+    return;//    throw "X out of range!";
+  
+  if(!inRange(y, 0, _height - 1))
+    return;//    throw "Y out of range!";
+  
+  // Draw as long as we can inside the frame with the buffer.
+  for(int i = 0; i < size; ++i)
+  {
+    const int pos = y * _width + x + i;
+    if(pos > _bufferSize - 1)
+      break;
+
+    _bufferChar[pos] = buf[i];
+    _bufferAttributes[pos].Foreground = fg;
+    _bufferAttributes[pos].Background = bg;
+  }
+}
+
+// Repositions the window in the canvas.
+// Size does not change.
+void Frame::Relocate(int x, int y)
+{
+  throw __func__;
+}
+
+// Sets the size of the window by adjusting width and height.
+// Position does not change.
+void Frame::SetSize(int width, int height)
+{
+  initBuffers();
+  throw __func__;
+}
+
+int Frame::Width() const
+{
+  return _height;
+}
+int Frame::Height() const
+{
+  return _height;
+}
+int Frame::PosX() const
+{
+  return _posX;
+}
+int Frame::PosY() const
+{
+  return _posY;
+}
+
+// Initializes the buffers again with the provided width and height.
+void Frame::initBuffers()
+{
+  _bufferSize = _width * _height;
+  delete[] _bufferChar;
+  delete[] _bufferAttributes;
+  _bufferChar = new char[_bufferSize];
+  _bufferAttributes = new Attribute[_bufferSize];
 }
 
 // Private member function that uses threads to translate
@@ -32,17 +104,27 @@ void Frame::Write(unsigned char c, int x, int y, ColorRGB)
 // canvas frame buffer.
 void Frame::translateToCanvas() const
 {
-  for(int i = 0; i < FrameHeight; ++i)
-    for(int j = 0; j < FrameWidth; ++j)
-    {
-      char buff[StepSize];
-      memcpy(buff, &_buffer[(i * FrameWidth + j) * StepSize], StepSize);
-      printf("\033[%d;%dH%s", PosY + i, PosX + j, buff);
-    }
+  // Does stuff.
 }
 
 // Returns the ID of this frame.
 int Frame::ID() const
 {
   return _id;
+}
+
+bool Frame::inRange(int val, int min, int max)
+{
+  if(val < min || val > max)
+    return false;
+  return true;
+}
+
+int Frame::wrap(int val, int max)
+{
+  val = (val % max);
+  if(val < 0) 
+    val += max;
+
+  return val;
 }
--- Test_08_MTWindowing/Frame.hpp
+++ Test_08_MTWindowing/Frame.hpp
@@ -1,35 +1,48 @@
 #include <memory>
+#include <string>
 #include "Color.hpp"
 
 
-class FrameManager;
-const char example[] = "\033[22;30ma";
-const int StepSize = sizeof(example) / sizeof(example[0]);
 
   /////////////////
  // Frame class //
 /////////////////
+class FrameManager;
 class Frame
 {
   friend FrameManager;
 public:
-  // Functions;
-  void Write(unsigned char c, int x, int y, ColorRGB color);
+  // Functions
+  void Write(char c, int x, int y, ColorRGB fg, ColorRGB bg);
+  void Write(char *c, int size, int x, int y, ColorRGB fg, ColorRGB bg);
   int ID() const;
+  ~Frame();
 
-  // Variables
-  int PosX;
-  int PosY;
-  int FrameWidth;
-  int FrameHeight;
+  // Variable access
+  void Relocate(int x, int y);
+  void SetSize(int width, int height);
+  int Width() const;
+  int Height() const;
+  int PosX() const;
+  int PosY() const;
 
 private:
+  // Private Ctor
+  Frame(int id, int x, int y, int width, int height);
+  
   // Private member functions
   void translateToCanvas() const;
-  Frame(int id, int x, int y, int width, int height);
+  void initBuffers();
+  bool inRange(int val, int min, int max);
+  int wrap(int val, int max);
 
   // Variables
+  int _width;
+  int _height;
+  int _posX;
+  int _posY;
   const int _id;
-  const int _bufferSize;
-  char *_buffer; 
+  int _bufferSize;
+  char *_bufferChar;
+  Attribute *_bufferAttributes; 
 };
